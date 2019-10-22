1ee5bdd76e8de1ce2dd8e8b01426189f47ddb93b
y.takahashi <ikmy0709@gmail.com>
2019-10-22 18:26:45 +0900

    Initial Commit

---
+++ SkeletonViewSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkeletonViewSample
+//
+//  Created by Takahashi Yuichiro on 2019/10/22.
+//  Copyright © 2019 Takahashi Yuichiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
